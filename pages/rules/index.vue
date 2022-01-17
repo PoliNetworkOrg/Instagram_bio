5ead347350914f0ea7dcf7c3af26e5d5e5b8a662
steven <steven@steven.com>
2022-01-17 23:02:57 +0100

    Update index.vue

---
+++ pages/rules/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <ul>
+      <li><a href="https://polinetwork.org/it/rules/">Regole</a></li>
+      <li><a href="https://polinetwork.org/en/rules/">Rules</a></li>
+    </ul>
+  </div>
+</template>
