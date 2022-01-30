63ef45aa1522dafd4ee33cfaf29fe3fa48d14641
steven9463912 <steven9463912@users.noreply.github.com>
2022-01-30 12:38:48 +0000

    deploy: 01c349f8aa022866a814fe2817ec8b5cb1749805

---
+++ _nuxt/static/1643546325/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Flink","\u002Frules","\u002Fen","\u002Fit","\u002Fen\u002Flink","\u002Fit\u002Flink","\u002Fen\u002Frules","\u002Fit\u002Frules"]})
