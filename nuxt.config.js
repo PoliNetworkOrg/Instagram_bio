3a1a57dcfc83bee74828a6f93c5ad1b838a17696
Eliaxie <elia.maggioni@gmail.com>
2022-02-01 12:54:49 +0100

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: 'PoliNetwork2',
+        title: 'PoliNetwork',
         htmlAttrs: {
             lang: 'en'
         },
